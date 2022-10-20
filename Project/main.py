ba7019e48e40536015bc148675939462b44861c7
kstepannikov <kirillstepannikov13@gmail.com>
2022-10-20 23:21:43 +0300

    hello

---
+++ Project/main.py
@@ -0,0 +1,8 @@
+chislo1 = 0
+chislo2 = 1
+vvod = int(input("Введите номер последнего элемента числа Фибаначчи: "))
+for i in range(vvod):
+sum= chislo1 + chislo2
+chislo1 = chislo2
+chislo2 = sum
+print("Ответ =",chislo1)
